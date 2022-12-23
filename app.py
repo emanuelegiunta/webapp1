5eff44f91de5efa381a346488dcbad04ac71dca1
Emanuele Giunta <emanuele.giunta@imdea.org>
2022-12-23 17:54:38 +0100

    Adaptation for `alwaysdata`
    * Changed route path to `/test`
    * Added some text to Hello World

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/test')
 def index():
-    return "hello world"
+    return "<h1>Hello World!</h1><body>This will be my website one day</body>"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True, port=8101)
