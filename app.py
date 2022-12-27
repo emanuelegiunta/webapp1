74cfaceca2bd03db2700c02800a637439cf026a2
Emanuele Giunta <emanuele.giunta@imdea.org>
2022-12-27 21:33:21 +0100

    Added pages for post content
    * Posts are accessed in `.../post_id`
    * Linked custom `style.css` to test on hosting website

--- app.py
+++ app.py
@@ -1,23 +1,58 @@
 import sqlite3
+import os.path
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 from settings import settings
 
 def get_db_connection():
+    '''Opens the database and return a `connection` object (entries of a table
+    can be iterated over this object. Rows will behave as dictionaries)
+    '''
+
     connection = sqlite3.connect('database.db')
     # Apparently. the way in which rows are later accessed (in this case in a 
     # python dict-like way
     connection.row_factory = sqlite3.Row
     return connection
 
+def get_post(post_id):
+    '''Get a post given a post ID, or else raises a NotFound error
+    '''
+
+    # For some reason we can't reuse the connection as we did before
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    post = connection.execute('SELECT * FROM posts WHERE id = ?', 
+                              (post_id,)).fetchone()
+    connection.close()
+
+    # Apparently post is either None or a string if the entry is non-empty
+    #  Thus we only 'match' against None and not-None
+    if post is None:
+        # Raise an Error 404
+        abort(404)
+
+    return post
+
+
 app = Flask(__name__)
 
+# by default settings.path = "/" so the line below is read as
+# `@app.route("/")`
 @app.route(settings.path)
 def index():
     connection = get_db_connection()
     # I'm not sure what fetchall does, but SELECT * FROM posts simply gets all
     #  entries from the table `posts`
     posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
     return render_template('index_extended.html', posts=posts)
+
+
+@app.route(os.path.join(settings.path, "<post_id>"))
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
 
 if __name__ == "__main__" and settings.path:
     app.run(debug=True)
