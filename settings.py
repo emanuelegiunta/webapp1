24d70795ffd7e768493994b53fb9eaec660620e0
Emanuele Giunta <emanuele.giunta@imdea.org>
2022-12-28 02:29:57 +0100

    Final fix for .gitignore

---
+++ settings.py
@@ -0,0 +1,14 @@
+class settings:
+    def __init__(self, path, debug):
+        ''' Settings initialization
+        path : Application sub-domain
+        debug: boolean, indicates whether debug-mode is on or not
+        '''
+        self.path = path
+        self.debug = debug
+
+# Sinlge object paradigm
+# `path` should end with a "/", eg: "/test/", "/mysite/app/"
+settings = settings(path="/", debug=True)
+
+
